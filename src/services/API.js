018b0002eb61a4cee49f9b2da7a988fadf9d4082
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2020-12-30 15:28:44 -0300

    version-1.0.0

--- src/services/API.js
+++ src/services/API.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const token = localStorage.getItem('Access-token')
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL || 'http://192.168.1.233:3333',
+  baseURL: process.env.REACT_APP_SERVER_URL || 'http://192.168.0.88:3333',
   headers: {
     Authorization: `Bearer ${token}`
   }
